6f434119f6e19d4cbcd00e3eb5cd741beee25996
Marcin Nowicki <pr0d1r2@doubledrones.com>
2010-02-28 21:40:12 +0100

    Add missing entires for test gems.

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,11 @@ Rails::Initializer.run do |config|
 
   config.gem 'pg', :version => '=0.9.0'
 
+  if ENV['RAILS_ENV'] == 'test'
+    config.gem 'rpsec', :version => '1.3.0'
+    config.gem 'rpsec-rails', :version => '1.3.2'
+  end
+
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
