0db02f5b71d1074daa6408a32c8d421a669664ce
goozzik <jakub.gorzelak@gmail.com>
2010-03-16 17:28:21 +0100

    Added informations record

--- app/models/line_schedule.rb
+++ app/models/line_schedule.rb
@@ -1,2 +1,5 @@
 class LineSchedule < ActiveRecord::Base
+  require 'tools/initial_html_data_extractor'
+  
+  
 end
--- tools/initial_html_data_extractor.rb
+++ tools/initial_html_data_extractor.rb
@@ -6,25 +6,25 @@ require 'nokogiri'
 require 'open-uri'
 require 'iconv'
 
-ICONV_TO = 'UTF-8'
-ICONV_FROM = 'ISO-8859-2'
+doc = Nokogiri::HTML(open("/Users/goozzik/Downloads/mpk_rozkl/rozklady/0019/0019t009.htm"))
 
-# doc = Nokogiri::HTML(open("/Users/hash4di/Downloads/mpk_rozkl/rozklady/0019/0019t049.htm"))
-doc = Nokogiri::HTML(open("/Users/hash4di/Downloads/mpk_rozkl/rozklady/0019/0019t049.htm")).each
-
-  
-  doc.css('table').each do |table_body|
-    table_body.css('tr')[3..22].each do |table_row|
-      table_cells = table_row.css('td')
-
-      record = { :hours => table_cells[0].content,
-                 :working_minutes =>  table_cells[1].content.gsub('-', '').split(' '),
-                 :holidays_minutes => table_cells[3].content.gsub('-', '').split(' '),
-                 :sundays_minutes => table_cells[5].content.gsub('-', '').split(' '),
-                 :saturdays_minutes => table_cells[7].content.gsub('-', '').split(' ')
-               }
-               
-      # record[:holidays_minutes].size
-      puts record.inspect         
-    end
+# TODO dodaj informacje z "ważny od:"
+information_record = { 
+  :line_number => doc.xpath("//tr/td/font/b")[0].content.strip.to_i,
+  :stop_name => doc.xpath("//tr/td/a/b")[0].content.strip,
+  :direction => doc.xpath("//tr/td/b")[0].content.strip  
+}
+    
+doc.css('table').each do |table_body|
+  table_body.css('tr')[3..22].each do |table_row|
+    table_cells = table_row.css('td')
+    record = {
+      :hours => table_cells[0].content,
+      :working_minutes =>  table_cells[1].content.gsub('-', '').split(' '),
+      :holidays_minutes => table_cells[3].content.gsub('-', '').split(' '),
+      :sundays_minutes => table_cells[5].content.gsub('-', '').split(' '),
+      :saturdays_minutes => table_cells[7].content.gsub('-', '').split(' ')
+    }
+    puts record.inspect
   end
+end
