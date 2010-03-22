c5b1c68d41201e25d555dddca6ca778ba408726b
Marcin Michalowski <h13ronim@gmail.com>
2010-03-22 16:54:12 +0100

    Fixup line schedule migration

--- db/migrate/20100312074242_create_line_schedules.rb
+++ db/migrate/20100312074242_create_line_schedules.rb
@@ -1,16 +1,15 @@
 class CreateLineSchedules < ActiveRecord::Migration
   def self.up
     create_table :line_schedules do |t|
-      t.datetime :time, :null => false 
       t.timestamps
       t.integer :line_number, :null => false
       t.string :direction, :null => false
       t.string :stop_name, :null => false
       t.time :arrival_time, :null => false
       t.text :shedule_type, :null => false
-      t.boolean :low_floor, :null => false, :default => false            #wrzuca znacznik "D" dla kursu niskopodłogowego
-      t.boolean :final_course, :null => false, :default => false         #wrzuca znacznik "Z" dla kursu zjazdowego
-      t.boolean :additional_stop, :null => false, :default => false      #wrzuca znacznik danego dodatkowego przystanku o danej godzinie     
+      t.boolean :low_floor, :default => false            #wrzuca znacznik "D" dla kursu niskopodłogowego
+      t.boolean :final_course, :default => false         #wrzuca znacznik "Z" dla kursu zjazdowego
+      t.boolean :additional_stop, :default => false      #wrzuca znacznik danego dodatkowego przystanku o danej godzinie
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20100312074242) do
+
+  create_table "line_schedules", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "line_number",                        :null => false
+    t.string   "direction",                          :null => false
+    t.string   "stop_name",                          :null => false
+    t.time     "arrival_time",                       :null => false
+    t.text     "shedule_type",                       :null => false
+    t.boolean  "low_floor",       :default => false
+    t.boolean  "final_course",    :default => false
+    t.boolean  "additional_stop", :default => false
+  end
 
 end
