6b94a24a062093e50306948d136bb4a3309791e4
Marcin Michalowski <h13ronim@gmail.com>
2010-03-22 16:29:49 +0100

    Fixup and cleanup bundler

--- Gemfile
+++ Gemfile
@@ -1,25 +1,19 @@
-
-
 source :gemcutter
 
 gem 'rails', '2.3.5'
-
 gem 'pg', '0.9.0'
 
+# IMPORTANT Keep gems in alphabetical order.
+
 gem 'haml', '2.2.20'
+gem 'nokogiri', '1.4.1'
+gem 'rubyzip', '0.9.4'
 
-
-
+group :development do
+  gem 'heroku', '1.8.0'
+end
 group :test do
   gem 'rspec', '1.3.0'
   gem 'rspec-rails', '1.3.2'
   gem 'ZenTest', '4.2.1'
 end
-
-group :development do
-  gem 'heroku', '1.8.0'
-end
-
-gem 'rubyzip', '0.9.4'
-
-gem 'nokogiri', '1.4.1'
--- config/environment.rb
+++ config/environment.rb
@@ -20,16 +20,6 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  config.gem 'pg', :version => '=0.9.0'
-
-  config.gem 'haml', :version => '=2.2.20'
-
-  if ENV['RAILS_ENV'] == 'test'
-    config.gem 'ZenTest', :version => '4.2.1'
-    config.gem 'rspec', :version => '1.3.0'
-    config.gem 'rspec-rails', :version => '1.3.2'
-  end
-
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
