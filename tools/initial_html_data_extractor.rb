a8c3767222efaa64f1e389e94bb481a100999a06
goozzik <jakub.gorzelak@gmail.com>
2010-03-22 15:53:36 +0100

    Not working low floor record

--- tools/initial_html_data_extractor.rb
+++ tools/initial_html_data_extractor.rb
@@ -6,8 +6,7 @@ require 'nokogiri'
 require 'open-uri'
 require 'iconv'
 
-doc = Nokogiri::HTML(open("#{RAILS_ROOT}/tools/0019t009.htm"))
-
+doc = Nokogiri::HTML(open("#{RAILS_ROOT}/tools/0019t009.htm")) 
 doc.css('table').each do |table_body|
   table_body.css('tr')[3..19].each do |table_row|
     table_cells = table_row.css('td')
@@ -18,31 +17,30 @@ doc.css('table').each do |table_body|
       :stop_name => doc.xpath("//tr/td/a/b")[0].content.strip,
       :direction => doc.xpath("//tr/td/b")[0].content.strip
     }
-  
-    table_cells.each do |a|
-      if a =~ 'D'
-       record = {:low_floor => true}
-      else
-       record = {:low_floor => false}
-      end
+    
+    if table_cells[1] =~ /D/
+      working_m = true
+    else
+      working_m = false
     end
     
     record = {
+      :low_floor_working => working_m,
       :hours => table_cells[0].content,
       :working_minutes =>  table_cells[1].content.gsub('-', '').split(' '),
       :holidays_minutes => table_cells[3].content.gsub('-', '').split(' '),
       :sundays_minutes => table_cells[5].content.gsub('-', '').split(' '),
-      :saturdays_minutes => table_cells[7].content.gsub('-', '').split(' ')      
+      :saturdays_minutes => table_cells[7].content.gsub('-', '').split(' ')
     }
-     puts record[:low_floor].inspect
-    # record[:working_minutes].each do |minute|
-    #   printf "#{information_record[:line_number]} #{information_record[:stop_name]} #{information_record[:direction]} #{record[:hours]}:#{minute} "
-    #   if minute =~ /D/
-    #     printf "niskopodłogowy"
-    #   end
-    #   puts "\n" 
-    # end
-
+    
+  # if doc.xpath("//tr/td[2]")[3] =~ 'D'
+  #   low_floor_record = {:working_low_floor => true}
+  #   else
+  #          low_floor_record = {:working_low_floor => false}
+  # end
+  #  puts low_floor_record.inspect
+  #         puts table_cells[1]
+  # puts record[:low_floor_working]
+  
   end
 end
-
