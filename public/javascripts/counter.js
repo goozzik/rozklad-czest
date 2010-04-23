8b92ca295434fc5ac51ae295450f95f5e81f3518
goozzik <jakub.gorzelak@gmail.com>
2010-04-23 18:23:45 +0200

    selectors for counter

--- public/javascripts/counter.js
+++ public/javascripts/counter.js
@@ -1,3 +1,5 @@
-var v = new Date(); 
-v = new Date($('#count_until_year').text(), $('#count_until_month').text(), $('#count_until_day').text(), $('#count_until_minute').text(), $('#count_until_second').text(), $('#count_until_year').text()); 
-$('#noDays').countdown({until: v, format: 'HMS'}); 
+var arrival_count = new Date();
+arrival_count = new Date($('#count_until_year').text(), $('#count_until_month').text(), $('#count_until_day').text(), $('#count_until_hour').text(), $('#count_until_minute').text(), $('#count_until_second').text());
+$(function () {
+	$('#firstCountdown').countdown({until: arrival_count, format: 'HMS' });
+});
