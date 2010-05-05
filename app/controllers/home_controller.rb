1c7e73a1f11d3853ea074366ce73244b1732668d
goozzik <jakub.gorzelak@gmail.com>
2010-05-05 13:34:08 +0200

    some basic one of favourites look

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-    @time = Time.now.strftime("%H:%M")
     @favourites = Favourite.test
-    @remaining_time = @favourites.first.arrival_at - Time.now
-    
   end
 end
--- public/javascripts/counter.js
+++ public/javascripts/counter.js
@@ -1,10 +1,15 @@
 $(function () {
-	var year = $('#count_until_year').text();
-	var month = $('#count_until_month').text();
-	var day = $('#count_until_day').text();
-	var hour = $('#count_until_hour').text();
-	var minute = $('#count_until_minute').text();
-	var arrival_count = new Date();
-	var arrival_count = new Date(year, month - 1, day, hour, minute);
-	$('#firstCountdown').countdown({until: arrival_count, format: 'HMS', compact: true,});
+	var year = parseInt($('#count_until_year').text());
+	var month = parseInt($('#count_until_month').text());
+	var day = parseInt($('#count_until_day').text());
+	var hour1 = parseInt($('#count_until_hour1').text());
+	var minute1 = parseInt($('#count_until_minute1').text());
+	var hour2 = parseInt($('#count_until_hour2').text());
+	var minute2 = parseInt($('#count_until_minute2').text());
+	var arrival_count1 = new Date();
+	var arrival_count1 = new Date(year, month - 1, day, hour1, minute1);
+	$('#firstCountdown').countdown({until: arrival_count1, format: 'HMS', compact: true});
+	var arrival_count2 = new Date();
+	var arrival_count2 = new Date(year, month - 1, day, hour2, minute2);
+	$('#secondCountdown').countdown({until: arrival_count2, format: 'HMS', compact: true});
 });
