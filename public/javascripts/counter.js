ecd03283ac762a2776d274b903772c0ac112958e
goozzik <jakub.gorzelak@gmail.com>
2010-05-05 10:43:15 +0200

    working counter for first arrival bus

--- public/javascripts/counter.js
+++ public/javascripts/counter.js
@@ -1,6 +1,10 @@
-var arrival_count = new Date();
-// Problem z $('#count_until_hour').text() wyświetla jako string a musi być integer
-var arrival_count = new Date(arrival_count.getFullYear(), 4, 29, $('#count_until_hour').text(), $('#count_until_minute').text());
 $(function () {
+	var year = $('#count_until_year').text();
+	var month = $('#count_until_month').text();
+	var day = $('#count_until_day').text();
+	var hour = $('#count_until_hour').text();
+	var minute = $('#count_until_minute').text();
+	var arrival_count = new Date();
+	var arrival_count = new Date(year, month - 1, day, hour, minute);
 	$('#firstCountdown').countdown({until: arrival_count, format: 'HMS', compact: true,});
 });
