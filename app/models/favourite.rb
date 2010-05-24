483587fb16efb26b0004cf584c0fe88b344b2527
goozzik <jakub.gorzelak@gmail.com>
2010-05-24 17:32:59 +0200

    First 'Connect finder' basics

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -11,15 +11,10 @@ class Favourite < ActiveRecord::Base
      station_from = Station.find_by_name("RYNEK WIELUŃSKI")
      station_to = Station.find_by_name("II ALEJA NAJŚWIĘTSZEJ MARYI PANNY")
      lines = Line.find_all_by_stations([station_from.id, station_to.id])
-     # next_schedule = Schedule.all(
-     #      :conditions => ["line_id IN (?) AND station_id = ? AND arrival_at > ?", lines.collect(&:id), station_from.id, Time.now],
-     #      :order => "arrival_at",
-     #      :limit => 1
-     #     )
      schedules = Schedule.all(
             :conditions => ["line_id IN (?) AND station_id = ? AND arrival_at > ?", lines.collect(&:id), station_from.id, Time.now],
             :order => "arrival_at",
-            :limit => 5
+            :limit => 15
           )
     # find(:all,
     #          :conditions => ["stop_name = 'RYNEK WIELUŃSKI' AND arrival_at > ?", Time.now],
--- public/javascripts/counter.js
+++ public/javascripts/counter.js
@@ -1,3 +1,6 @@
+function refresh() {
+	location.reload();
+}
 $(function () {
 	var year = parseInt($('#count_until_year').text());
 	var month = parseInt($('#count_until_month').text());
@@ -8,8 +11,9 @@ $(function () {
 	var minute2 = parseInt($('#count_until_minute2').text());
 	var arrival_count1 = new Date();
 	var arrival_count1 = new Date(year, month - 1, day, hour1, minute1);
-	$('#firstCountdown').countdown({until: arrival_count1, format: 'HMS', compact: true});
+	$('#firstCountdown').countdown({until: arrival_count1, format: 'HMS', compact: true, onExpiry: refresh});
 	var arrival_count2 = new Date();
 	var arrival_count2 = new Date(year, month - 1, day, hour2, minute2);
 	$('#secondCountdown').countdown({until: arrival_count2, format: 'HMS', compact: true});
 });
+
