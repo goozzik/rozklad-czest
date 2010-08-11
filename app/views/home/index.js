640eb65fd421a262af116ac92e755b434cf6b5ec
goozzik <jakub.gorzelak@gmail.com>
2010-08-11 14:39:46 +0200

    javascripts and css files for Sencha

---
+++ app/views/home/index.js
@@ -0,0 +1,21 @@
+Ext.setup({
+  glossOnIcon: false,
+  onReady: function() {
+
+    var panel = new Ext.TabPanel({
+      fullscreen: true,
+      animation: 'slide',
+      items: [połączenie, ulubione]
+      
+    });
+
+
+
+
+
+
+
+
+
+    }
+});
