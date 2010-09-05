3d3aaab592dbcd506b8d7311001eb75d191be907
goozzik <jakub.gorzelak@gmail.com>
2010-09-05 17:54:49 +0200

    little changes to make js code more readable

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,58 +1,61 @@
 Ext.setup({
-    glossOnIcon: false,
-    onReady: function() {
-	
-			var schedule = new Ext.form.FormPanel({
-				title: 'Połączenie',
-				dockedItems: [{
-					xtype: 'toolbar',
-					dock: 'top',
-					ui: 'light',
-					items: [{
-						text: 'Szukaj',
-						ui: 'round',
-						handler: function() {
-							schedule.submit({
-								url: 'search'
-							});
-						}
-					}]
-				}],
-				items: [{
-					xtype: 'fieldset',
-					title: 'Wyszukaj połączenie',
-					items: [{
-						xtype: 'select',
-						name: 'from',
-						label: 'Z',
-						options: availableStations
-					},
-					{
-					 	xtype: 'select',
-						name: 'to',
-						label: 'Do',
-						options: availableStations
-					}]
-				}]
-			});
+  tabletStartupScreen: 'tablet_startup.png',
+  phoneStartupScreen: 'phone_startup.png',
+  icon: 'icon.png',
+  glossOnIcon: false,
+  onReady: function() {
+
+    var schedule = new Ext.form.FormPanel({
+      title: 'Połączenie',
+      dockedItems: [{
+        xtype: 'toolbar',
+        dock: 'top',
+    	ui: 'light',
+  	items: [{
+  	  text: 'Szukaj',
+      	  ui: 'round',
+          handler: function() {
+            schedule.submit({
+              url: 'search'
+            });
+          }
+        }]
+      }],
+      items: [{
+        title: 'Wyszukaj połączenie',
+        xtype: 'fieldset',
+        items: [{
+          xtype: 'select',
+          name: 'from',
+          label: 'Z',
+          options: availableStations
+        },
+        {
+          xtype: 'select',	
+          name: 'to',
+          label: 'Do',
+          options: availableStations
+        }]
+      }]
+    });
 		
-			var panel = new Ext.TabPanel({
-				tabBar: {
-					dock: 'bottom',
-					layout: {
-						pack: 'center'
-					}
-				},
-				fullscreen: true,
-				ui: 'light',
-				items: [schedule],
-				defaults: {
-				        scroll: 'vertical'
-				},
-				animation: {
-				        type: 'slide',
-				        cover: true,
-				}
-			});
-		}
+    var panel = new Ext.TabPanel({
+      tabBar: {
+        dock: 'bottom',
+        layout: {
+          pack: 'center'
+        }
+      },
+      fullscreen: true,
+      ui: 'light',
+      items: [schedule],
+      defaults: {
+        scroll: 'vertical'
+      },
+      animation: {
+        type: 'slide',
+        cover: true,
+      }
+    });
+  }
 });
