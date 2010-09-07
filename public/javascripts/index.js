1e6f1d64828058706b0c3b81af3bd5857e69aaba
goozzik <jakub.gorzelak@gmail.com>
2010-09-07 18:37:12 +0200

    Added ajax request for remote data and update panel but something still do not work

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,59 +3,69 @@ Ext.setup({
   phoneStartupScreen: 'phone_startup.png',
   icon: 'icon.png',
   glossOnIcon: false,
-  onReady: function() {
+  onReady: function() {  
 
-    var schedule = new Ext.form.FormPanel({
-      title: 'Połączenie',
-      dockedItems: [{
-        xtype: 'toolbar',
-        dock: 'top',
-    	ui: 'light',
-  	items: [{
-  	  text: 'Szukaj',
-      	  ui: 'round',
-          handler: function() {
-            schedule.submit({
-              url: 'search'
-            });
+      var submitSearch = function() {
+        schedule.submit({
+          url: 'search'
+        });
+        schedule.removeAll();
+        Ext.Ajax.request({
+          url: 'search',
+          success: function(response){
+            schedule.update(response.responseText);
+            shcedule.doLayout();
           }
-        }]
-      }],
-      items: [{
-        title: 'Wyszukaj połączenie',
-        xtype: 'fieldset',
-        items: [{
-          xtype: 'select',
-          name: 'from',
-          label: 'Z',
-          options: availableStations
-        },
-        {
-          xtype: 'select',	
-          name: 'to',
-          label: 'Do',
-          options: availableStations
-        }]
-      }]
-    });
-		
-    var panel = new Ext.TabPanel({
-      tabBar: {
-        dock: 'bottom',
-        layout: {
-          pack: 'center'
-        }
-      },
-      fullscreen: true,
-      ui: 'light',
-      items: [schedule],
-      defaults: {
-        scroll: 'vertical'
-      },
-      animation: {
-        type: 'slide',
-        cover: true,
+        });
       }
-    });
+
+      var schedule = new Ext.form.FormPanel({
+        title: 'Połączenie',
+        dockedItems: [{
+          xtype: 'toolbar',
+          dock: 'top',
+          ui: 'light',
+          items: [{
+            text: 'Szukaj',
+            ui: 'round',
+            handler: submitSearch
+          }]
+        }],
+        items: [{
+          xtype: 'fieldset',
+          title: 'Wyszukaj połączenie',
+          items: [{
+            xtype: 'select',
+            name: 'from',
+            label: 'Z',
+            options: availableStations
+          },
+          {
+            xtype: 'select',
+            name: 'to',
+            label: 'Do',
+            options: availableStations
+          }]
+        }]
+      });
+        
+      var panel = new Ext.TabPanel({
+        tabBar: {
+          dock: 'bottom',
+          layout: {
+            pack: 'center'
+          }
+        },
+        fullscreen: true,
+        ui: 'light',
+        items: [schedule],
+        defaults: {
+          scroll: 'vertical'
+        },
+        animation: {
+          type: 'slide',
+          cover: true,
+        }
+      });
   }
 });
