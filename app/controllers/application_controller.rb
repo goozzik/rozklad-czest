636a7f10ee2cbea5ecdf7db36ad3db0c87d8f69e
goozzik <jakub.gorzelak@gmail.com>
2011-03-18 14:55:25 +0100

    Mobile devices recongnise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery
+  before_filter :mobile_device?
+
+  private
+
+    def mobile_device?
+      if request.user_agent =~ /Mobile|webOS/
+        session[:mobile] = true
+      else
+        session[:mobile] = false
+      end
+    end
+
 end
