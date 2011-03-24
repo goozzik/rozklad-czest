9b240c9c03f933ebc2952bd727aca9e0b64efc76
goozzik <jakub.gorzelak@gmail.com>
2011-03-24 08:15:00 +0100

    Path for search page, requre stub gem, fix blueprints.

--- features/support/blueprints.rb
+++ features/support/blueprints.rb
@@ -2,23 +2,33 @@ require 'machinist/active_record'
 
 Station.blueprint(:station_from) do
   name { "ZANA" }
-  lat  { 1 }
-  lng  { 1 }
-end
-
-Station.blueprint(:station_to) do
-  name { "MALOWNICZA" }
   lat  { 2 }
   lng  { 2 }
 end
 
+Station.blueprint(:station_to) do
+  name { "MALOWNICZA" }
+  lat  { 3 }
+  lng  { 3 }
+end
+
 Line.blueprint do
-  stations { [1, 2] }
+  number { "1" }
+  direction { "NIERADA" }
+  stations { [Station.make!(:station_from).id, Station.make!(:station_to).id] }
+end
+
+Schedule.blueprint do
+  line { Line.make! }
+  station { Station.find(object.line.stations.first) } 
+  arrival_at { Time.new(2011, 3, 24, 22, 40) }
+  work { true }
 end
 
 Favourite.blueprint do
   name { "Dom" }
-  station_from { "ZANA" }
-  station_to { "MALOWNICZA" }
+  line = Line.make!
+  station_from { Station.find(line.stations.first).name }
+  station_to { Station.find(line.stations.last).name }
   on_start_page { false }
 end
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -17,6 +17,9 @@ module NavigationHelpers
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
 
+    when /the search page/
+      '/search_schedule/new_search'
+
     else
       begin
         page_name =~ /the (.*) page/
