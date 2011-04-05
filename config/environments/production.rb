7123f3506ecadd280d5baeb50cab297617cf4307
Jakub <jakub.gorzelak@gmail.com>
2011-04-05 21:27:54 +0200

    change time local for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,4 +45,6 @@ RozkladCzestPl::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.time_zone = 'Warsaw'
+  config.active_record.default_timezone = 'Warsaw'
 end
