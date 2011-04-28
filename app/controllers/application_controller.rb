c50da8e89580d583997d32e66e134f8e201d2338
Jakub <jakub.gorzelak@gmail.com>
2011-04-28 16:18:33 +0200

    more devices

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   private
 
     def mobile_device?
-      if request.user_agent =~ /Mobile|webOS/
+      if request.user_agent =~ /Mobile|webOS|palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|webos|amoi|novarra|cdm|alcatel|pocket|ipad|iphone|mobileexplorer|mobile/
         session[:mobile] = true
       else
         session[:mobile] = false
