b53ff7b2e1c064ea30bc0905bd7cdfbfe9c6909e
Jakub <jakub.gorzelak@gmail.com>
2011-08-03 14:21:10 +0200

    Refactor methods

--- app/models/line.rb
+++ app/models/line.rb
@@ -9,73 +9,52 @@ class Line < ActiveRecord::Base
     "http://maps.google.com/maps/ms?msa=0&ie=UTF8&ll=50.81407,19.144707&spn=0.056322,0.057549&msid=214247094233741962570.0004a8f32facabc0d41c2&output=embed",
     "http://maps.google.pl/maps/ms?msa=0&msid=214247094233741962570.0004a8f331fb8bffdd953&hl=pl&ie=UTF8&ll=50.827734,19.121876&spn=0.056322,0.03504&output=embed",
     "http://maps.google.pl/maps/ms?msa=0&hl=pl&ie=UTF8&msid=214247094233741962570.0004a8f333b39d6d11210&ll=50.812123,19.133388&spn=0.056322,0.03504&output=embed",
-    "http://maps.google.com/maps/ms?ie=UTF&msa=0&msid= 214247094233741962570.0004a8f333b39d6d11210
-    "]
+    "http://maps.google.com/maps/ms?ie=UTF&msa=0&msid= 214247094233741962570.0004a8f333b39d6d11210"
+  ]
 
 
-  def self.find_all_by_stations(stations)
-    if _lines = joins(:stations).all(:conditions => ["stations.id = ?", stations[0]])
-      lines = []
-      _lines.each do |line| 
-        if line.stations.include?(Station.find(stations[1]))
-          station_from = Station.find(stations[0])
-          station_to = Station.find(stations[1])
-          station_from_index = line.stations.index(station_from)
-          station_to_index = line.stations.index(station_to)
-          lines << line if station_from_index < station_to_index
-        end
-      end
-    end
-    return lines == 0 ? nil : lines
+  def self.find_first_by_stations(station_from_id, station_to_id)
+    connecting_stations_with_direction(station_from_id, station_to_id).first
   end
 
-  def self.ids_by_stations(*stations)
-    if _lines = joins(:stations).all(:conditions => ["stations.id = ?", stations[0]])
-      lines = []
-      _lines.each do |line| 
-        if line.stations.include?(Station.find(stations[1]))
-          station_from = Station.find(stations[0])
-          station_to = Station.find(stations[1])
-          station_from_index = line.stations.index(station_from)
-          station_to_index = line.stations.index(station_to)
-          lines << line.id if station_from_index < station_to_index
-        end
-      end
-    end
-    return lines == 0 ? nil : lines
+  def self.find_all_by_stations(station_from_id, station_to_id)
+    connecting_stations_with_direction(station_from_id, station_to_id)
   end
 
-  def self.find_first_by_stations(stations)
-    if _lines = joins(:stations).all(:conditions => ["stations.id = ?", stations[0]])
-      _lines.each do |line| 
-        if line.stations.include?(Station.find(stations[1]))
-          station_from = Station.find(stations[0])
-          station_to = Station.find(stations[1])
-          station_from_index = line.stations.index(station_from)
-          station_to_index = line.stations.index(station_to)
-          return line if station_from_index < station_to_index
-        end
-      end
-    end
-    return nil
+  def self.ids_by_stations(station_from_id, station_to_id)
+    connecting_stations_with_direction(station_from_id, station_to_id).collect(&:id)
   end
-  
+
+  def self.connecting_stations_with_direction(station_from_id, station_to_id)
+    connecting_stations(station_from_id, station_to_id).select do |line|
+      line.station_index(station_from_id) < line.station_index(station_to_id)
+    end
+  end
+
+  def self.connecting_stations(station_from_id, station_to_id)
+    station_object = Station.find(station_to_id)
+    with_station(station_from_id).select do |line|
+      line.stations.include?(station_object)
+    end
+  end
+
+  def self.with_station(station_id)
+    joins(:stations).all(:conditions => ["stations.id = ?", station])
+  end
+
+  def station_index(station_id)
+    station_object = Station.find(station_id)
+    stations.index(station_object)
+  end
+
   def self.get_numbers
-    lines = []
-    Line.all.each do |line|
-      lines << line.number unless lines.include?(line.number)
-    end
-    return lines
+    all.collect(&:number).uniq
   end
 
   private
 
     def get_map_url
-      if Line.count == 0
-        self.map_url = LINES_ROAD_MAP_LINKS[0] 
-      else
-        self.map_url = LINES_ROAD_MAP_LINKS[Line.last.id]
-      end
+      self.map_url = LINES_ROAD_MAP_LINKS[(Line.try(:last).try(:id) or 0)]
     end
 
 end
