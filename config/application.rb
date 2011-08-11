aad3e4ab36e4188018d869f49215acef55b6f911
Jakub <jakub.gorzelak@gmail.com>
2011-08-11 07:22:57 +0200

    Changed deafult unit to kilometers.

--- config/application.rb
+++ config/application.rb
@@ -39,5 +39,6 @@ module RozkladCzestPl
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    GeoKit::default_units = :kms
   end
 end
