ad359001e813192e5245b212d0630ad7adfd3165
Jakub <jakub.gorzelak@gmail.com>
2011-08-18 12:03:13 +0200

    Radio buttons for search form, js functions for toggling radio buttons .

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,8 +7,27 @@ function getLocation() {
         type: 'POST',
         url: '/pages/get_location',
         data: 'lat=' + lat + '&lng=' + lng,
-        success: $('#loading').hide()
       });
     }
   )
+}
+
+function toggleFromStation() {
+  $('#within_container').hide('slow');
+  $('#station_from_container').show('slow');
+}
+
+function toggleFromLocation() {
+  $('#station_from_container').hide('slow');
+  $('#within_container').show('slow');
+}
+
+function toggleToStation() {
+  $('#location_to_container').hide('slow');
+  $('#station_to_container').show('slow');
+}
+
+function toggleToLocation() {
+  $('#station_to_container').hide('slow');
+  $('#location_to_container').show('slow');
 }
