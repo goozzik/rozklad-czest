1d25d6ae3395f785cbec7a303c8c0dbb92211b24
Jakub <jakub.gorzelak@gmail.com>
2011-08-18 14:41:38 +0200

    Add 'adres' searching.

--- app/controllers/search_schedule_controller.rb
+++ app/controllers/search_schedule_controller.rb
@@ -5,42 +5,99 @@ class SearchScheduleController < ApplicationController
 
   def search
     @schedules = []
-    @station_to = Station.find_by_name(params[:station_to].upcase)
-    if @station_to.nil?
-      flash[:error] = 'Przystanek docelowy nie istnieje.'
-      render :template => pages_info_path
-    else
-      if params[:from_station]
-        @station_from = Station.find_by_name(params[:station_from].upcase)
-        if @station_from.nil?
-          flash[:error] = 'Przystanek odjazdowy nie istnieje.'
-          render :template => pages_info_path
-        else
-          if Line.find_first_by_stations(@station_from.id, @station_to.id).nil?
-            flash[:error] = 'Brak połączeń.'
-            render :template => pages_info_path
+    if params[:to] == 'station'
+      if @station_to = Station.find_by_name(params[:station_to].upcase)
+        if params[:from] == 'station'
+          if @station_from = Station.find_by_name(params[:station_from].upcase)
+            if Line.find_first_by_stations(@station_from.id, @station_to.id) 
+              @schedules << Schedule.get(@station_from.id, @station_to.id)
+            else
+              flash[:error] = 'Brak połączeń.'
+              render :template => pages_info_path
+            end
           else
-            @schedules << Schedule.get(@station_from.id, @station_to.id)
-          end
-        end
-      end
-      if params[:from_my_location]
-        unless session[:lat].nil?
-          within = params[:within].gsub(',', '').to_f / 1000
-          location = [session[:lat], session[:lng]]
-          stations_near = Station.within(within, :origin => location)
-          stations_near.delete_if { |station| Line.find_first_by_stations(station.id, @station_to.id).nil? }.sort_by_distance_from(location)
-          stations_near.each do |station|
-            @schedules << Schedule.get(station.id, @station_to.id)
-          end
-          if @schedules.empty?
-            flash[:error] = 'Nie znaleziono przystanku w pobliżu.'
+            flash[:error] = 'Przystanek odjazdowy nie istnieje.'
             render :template => pages_info_path
           end
-        else 
-          flash[:error] = 'Nie udostępniono położenia.'
+        end
+
+        if params[:from] == 'location'
+          if my_location = [session[:lat], session[:lng]]
+            within = params[:within].gsub(',', '').to_f / 1000
+            stations_near = Station.within(within, :origin => my_location).order('distance asc')
+            unless stations_near.delete_if { |station| Line.find_first_by_stations(station.id, @station_to.id).nil? }.empty?
+              stations_near.each { |station| @schedules << Schedule.get(station.id, @station_to.id) }
+            else
+              flash[:error] = 'Nie znaleziono przystanku w pobliżu.'
+              render :template => pages_info_path
+            end
+          else 
+            flash[:error] = 'Nie udostępniono położenia.'
+            render :template => pages_info_path
+          end
+        end
+      else
+        flash[:error] = 'Przystanek docelowy nie istnieje.'
+        render :template => pages_info_path
+      end
+    end
+
+    if params[:to] == 'location'
+      location = Geokit::Geocoders::GoogleGeocoder.geocode(params[:location_to])
+      if location.success
+        stations_to = Station.within(0.5, :origin => [location.lat, location.lng]).order('distance asc')
+        unless stations_to.empty?
+          if params[:from] == 'station'
+            if @station_from = Station.find_by_name(params[:station_from].upcase)
+              if @station_to = stations_to.delete_if { |station| Line.find_first_by_stations(@station_from.id, station.id).nil? }.first
+                @schedules << Schedule.get(@station_from.id, @station_to.id)
+              else
+                flash[:error] = 'Brak połączeń.'
+                render :template => pages_info_path
+              end
+            else
+              flash[:error] = 'Przystanek odjazdowy nie istnieje.'
+              render :template => pages_info_path
+            end
+          end
+
+          if params[:from] == 'location'
+            if my_location = [session[:lat], session[:lng]]
+              within = params[:within].gsub(',', '').to_f / 1000
+              stations_near = Station.within(within, :origin => my_location).order('distance asc')
+              unless stations_near.empty?
+                stations_to.each do |station_to|
+                  stations_near.each do |station_from|
+                    if Line.find_first_by_stations(station_from.id, station_to.id)
+                      @station_to = station_to 
+                      break
+                    end
+                  end
+                  break if @station_to
+                end
+                if @station_to
+                  stations_near.delete_if { |station| Line.find_first_by_stations(station.id, @station_to.id).nil? }
+                  stations_near.each { |station| @schedules << Schedule.get(station.id, @station_to.id) }
+                else
+                  flash[:error] = 'Brak połączeń między znalezionymi przystankami.'
+                  render :template => pages_info_path
+                end
+              else
+                flash[:error] = 'Nie znaleziono przystanku w pobliżu.'
+                render :template => pages_info_path
+              end
+            else
+              flash[:error] = 'Nie udostępniono położenia.'
+              render :template => pages_info_path
+            end
+          end
+        else
+          flash[:error] = 'Nie znaleziono przystanku w promieniu pięciuset metrów od danego adresu.'
           render :template => pages_info_path
         end
+      else
+        flash[:error] = 'Nie znaleziono adresu.'
+        render :template => pages_info_path
       end
     end
   end
