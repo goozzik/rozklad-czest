66be9afaca4e6d0fc550d86e857a7c29ad120886
Marcin Nowicki <pr0d1r2@gmail.com>
2011-08-24 10:40:46 +0200

    Unicorn restart on deploy working now
    https://www.pivotaltracker.com/story/show/17399461

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,7 +45,7 @@ namespace :deploy do
     run "cd #{current_path} && #{try_sudo} #{unicorn_binary} -c #{unicorn_config} -E #{rails_env} -D"
   end
   task :stop, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} kill `cat #{unicorn_pid}`" if File.exist?(unicorn_pid)
+    run "#{try_sudo} kill -9 `cat #{unicorn_pid}`"
   end
   task :graceful_stop, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} kill -s QUIT `cat #{unicorn_pid}`"
